caee13b32b7a08d764433b61de738b0939ad0f7b
arigatoha <anton.shevchuk@majorel.com>
2025-12-01 10:16:25 +0000

    modified cmake to store stderr to logs.txt(TODO need to change the path for logs thogit add -A), also added alias make run to easier run the program.

---
+++ CMakeFiles/webserv.dir/cmake_clean.cmake
@@ -0,0 +1,38 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/webserv.dir/src/AConfigBlock.cpp.o"
+  "CMakeFiles/webserv.dir/src/AConfigBlock.cpp.o.d"
+  "CMakeFiles/webserv.dir/src/AParser.cpp.o"
+  "CMakeFiles/webserv.dir/src/AParser.cpp.o.d"
+  "CMakeFiles/webserv.dir/src/Client.cpp.o"
+  "CMakeFiles/webserv.dir/src/Client.cpp.o.d"
+  "CMakeFiles/webserv.dir/src/Config.cpp.o"
+  "CMakeFiles/webserv.dir/src/Config.cpp.o.d"
+  "CMakeFiles/webserv.dir/src/Environment.cpp.o"
+  "CMakeFiles/webserv.dir/src/Environment.cpp.o.d"
+  "CMakeFiles/webserv.dir/src/Location.cpp.o"
+  "CMakeFiles/webserv.dir/src/Location.cpp.o.d"
+  "CMakeFiles/webserv.dir/src/ParseConfig.cpp.o"
+  "CMakeFiles/webserv.dir/src/ParseConfig.cpp.o.d"
+  "CMakeFiles/webserv.dir/src/ParseRequest.cpp.o"
+  "CMakeFiles/webserv.dir/src/ParseRequest.cpp.o.d"
+  "CMakeFiles/webserv.dir/src/RequestHandler.cpp.o"
+  "CMakeFiles/webserv.dir/src/RequestHandler.cpp.o.d"
+  "CMakeFiles/webserv.dir/src/ServerConfig.cpp.o"
+  "CMakeFiles/webserv.dir/src/ServerConfig.cpp.o.d"
+  "CMakeFiles/webserv.dir/src/StringUtils.cpp.o"
+  "CMakeFiles/webserv.dir/src/StringUtils.cpp.o.d"
+  "CMakeFiles/webserv.dir/src/httpRequest.cpp.o"
+  "CMakeFiles/webserv.dir/src/httpRequest.cpp.o.d"
+  "CMakeFiles/webserv.dir/src/main.cpp.o"
+  "CMakeFiles/webserv.dir/src/main.cpp.o.d"
+  "CMakeFiles/webserv.dir/src/server.cpp.o"
+  "CMakeFiles/webserv.dir/src/server.cpp.o.d"
+  "logs.txt"
+  "webserv"
+  "webserv.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/webserv.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
